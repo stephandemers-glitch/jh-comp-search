2ad0db99b9fe61a0630fb9752b2549db6ea02cd8
stephandemers-glitch <stephan.demers@gmail.com>
2026-06-23 17:02:49 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CompSearch from "./CompSearch.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <CompSearch />
-  </React.StrictMode>
-);
+import App from "./App.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode><App /></React.StrictMode>);
